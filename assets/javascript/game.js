666f3ca28cfaae6e973cfd66a83274eaed0d7f1e
Roland Willis <rolandwillisiv@gmail.com>
2018-03-21 11:47:35 -0700

    Game script is functioning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,104 @@
-var puzzleWords = ["happy","tree","mouse","cup","phone","computer"];
-var gameState = ["playing","You Win", "You Lose"];
+var puzzleWords = [""];
+var gameState = [""];
 var i = 0;
-var currentWord = puzzleWords[Math.floor(Math.random() * puzzleWords.length)];
+var currentWord = "";
 var solution = [""];
 var playerGuesses = [""];
-var guessesLeft = 10;
+var guessesLeft = 0;
 
-document.getElementById("game-state").innerHTML = gameState[0];
-
-solution.length = 0;
-for (i=0; i < currentWord.length; i++) {
-    solution.push(" _ ");
-}
-
-for (i=0; i < solution.length; i++) {
-    document.getElementById("blanks").innerHTML += solution[i];
-}
-
-document.getElementById("lives").innerHTML = guessesLeft;
+initiateGame();
 
 document.onkeyup = function(event) {
 
     // Determines which key was pressed.
     var userGuess = event.key;
 
-    if (playerGuesses.indexOf(userGuess) === -1 && guessesLeft > 0) {
-        playerGuesses.push(userGuess);
-
-        document.getElementById("guessWin").innerHTML = "";
-
-        for (i = 0; i < playerGuesses.length; i++){
-            document.getElementById("guessWin").innerHTML += playerGuesses[i] + " | "; 
-        }
+        if(allLetter(userGuess)) {
+            if (playerGuesses.indexOf(userGuess) === -1 && guessesLeft > 0) {
+                playerGuesses.push(userGuess);
         
-        if (currentWord.includes(userGuess)) {
-
-            solution.length = 0;
-
-            for (i = 0; i < currentWord.length; i++) {
-                if (playerGuesses.indexOf(currentWord.charAt(i)) === -1) {
-
-                    solution.push(" _ ");
-                } else {
-                    solution.push(currentWord.charAt(i));
+                document.getElementById("guessWin").innerHTML = "";
+        
+                for (i = 0; i < playerGuesses.length; i++){
+                    document.getElementById("guessWin").innerHTML += playerGuesses[i] + " | "; 
                 }
-            }
-
-            document.getElementById("blanks").innerHTML = "";
-            for (i=0; i < solution.length; i++) {
-                document.getElementById("blanks").innerHTML += solution[i];
-            }
-
-        } else {
-            if (guessesLeft > 0) {
-                guessesLeft--;
-                document.getElementById("lives").innerHTML = "";
-                document.getElementById("lives").innerHTML = guessesLeft;
+                
+               if (currentWord.includes(userGuess)) {
+        
+                    updateSolution();
+        
+                } else {
+        
+                    updateLives();
+                    
+                }
+        
             }
         }
 
-    }
-
-    if (guessesLeft < 1) {
-        document.getElementById("game-state").innerHTML = gameState[2];
-    }
+    updateGameState();
     
+}
 
+function initiateGame() {
+    puzzleWords = ["happy","tree","mouse","cup","phone","computer"];
+    gameState = ["playing","You Win", "You Lose"];
+    currentWord = puzzleWords[Math.floor(Math.random() * puzzleWords.length)];
+    guessesLeft = 10;
+    document.getElementById("lives").innerHTML = guessesLeft;
+    updateSolution();
+    updateGameState();
+}
+
+function allLetter(inputtxt) {
+    var input = inputtxt;
+
+    var letters = /^[A-Za-z]+$/;
+
+    if (input.match(letters)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function updateLives() {
+    if (guessesLeft > 0) {
+        guessesLeft--;
+        document.getElementById("lives").innerHTML = "";
+        document.getElementById("lives").innerHTML = guessesLeft;
+    }
+}
+
+function updateSolution() {
+
+    solution.length = 0;
+
+    for (i = 0; i < currentWord.length; i++) {
+        if (playerGuesses.indexOf(currentWord.charAt(i)) === -1) {
+    
+            solution.push(" _ ");
+        } else {
+            solution.push(" " + currentWord.charAt(i) + " ");
+        }
+    }
+
+    document.getElementById("blanks").innerHTML = "";
+
+    for (i=0; i < solution.length; i++) {
+        document.getElementById("blanks").innerHTML += solution[i];
+    }
+}
+
+function updateGameState() {
+
+    if (solution.indexOf(" _ ") === -1) {
+        document.getElementById("game-state").innerHTML = gameState[1];
+        guessesLeft = 0;
+    } else if (guessesLeft < 1) {
+        document.getElementById("game-state").innerHTML = gameState[2];
+    } else if (guessesLeft > 0 && guessesLeft < 11) {
+        document.getElementById("game-state").innerHTML = gameState[0];
+    }
 }
